3a8175065d0cbcf3edb30c93260af0c66d9eace6
Stefan Davis <stefan@turnkeylinux.org>
2020-06-02 13:28:33 +1000

    Update inithooks for 16.0

--- overlay/usr/lib/inithooks/bin/dokuwiki.py
+++ overlay/usr/lib/inithooks/bin/dokuwiki.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """Set Dokuwiki admin password and email
 
 Option:
@@ -17,20 +17,21 @@ from dialog_wrapper import Dialog
 
 def usage(s=None):
     if s:
-        print >> sys.stderr, "Error:", s
-    print >> sys.stderr, "Syntax: %s [options]" % sys.argv[0]
-    print >> sys.stderr, __doc__
+        print("Error:", s, file=sys.stderr)
+    print("Syntax: %s [options]" % sys.argv[0], file=sys.stderr)
+    print(__doc__, file=sys.stderr)
     sys.exit(1)
 
 def genpasswd(password):
-    return check_output(['openssl', 'passwd', '-1', password]).rstrip()
+    return check_output(['openssl', 'passwd', '-1', password],
+            encoding='utf8').rstrip()
     
 
 def main():
     try:
         opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
                                        ['help', 'pass=', 'email='])
-    except getopt.GetoptError, e:
+    except getopt.GetoptError as e:
         usage(e)
 
     password = ""
@@ -64,18 +65,19 @@ def main():
     hashpass = genpasswd(password)
 
     lines = []
-    for line in file(authfile).readlines():
-        line = line.strip()
-        if not line or line.count(":") < 4:
-            lines.append(line)
-	    continue
+    with open(authfile, 'r') as fob:
+        for line in fob:
+            line = line.strip()
+            if not line or line.count(":") < 4:
+                lines.append(line)
+                continue
 
-        username, password, name, mailaddr, groups = line.split(":")
-        if username == "admin":
-            password = hashpass
-            mailaddr = email
+            username, password, name, mailaddr, groups = line.split(":")
+            if username == "admin":
+                password = hashpass
+                mailaddr = email
 
-        lines.append(":".join([username, password, name, mailaddr, groups]))
+            lines.append(":".join([username, password, name, mailaddr, groups]))
 
     with open(authfile, 'w') as fob:
         fob.write('\n'.join(lines))
